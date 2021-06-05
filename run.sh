d94052b5a13e2259aad3aa0f842506c1e0b9f57b
khsinliao <r09921096@ntu.edu.tw>
2021-06-06 01:21:15 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@ root="/tmp/tungwen"
 image="disk_image"
 image_size=64K
 inodes=16
-mkdir ${root}
+#mkdir ${root}
 # Assumptions
 # we assume the  script is ran in the working repository
 # We assume that root is an existing empty directory
 
 #prepare root and image
-#fusermount -u ${root}
+fusermount -u ${root}
 truncate -s ${image_size} ${image}
 
 # format and mount fs image
